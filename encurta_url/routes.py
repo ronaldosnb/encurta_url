8125af3ff449e741c5afc24953e22a3087bf3331
Ronaldo Silva Nogueira Batista <ronaldosnbatista@gmail.com>
2025-07-17 23:40:59 -0300

    criação rota para index

---
+++ encurta_url/routes.py
@@ -0,0 +1,7 @@
+from encurta_url import app, db
+from encurta_url.models import Url
+from flask import render_template, request, redirect, url_for
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
