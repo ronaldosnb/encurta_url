579a9349aa1fc6d1e6c098d907ff24d7c0f6fca7
Ronaldo Silva Nogueira Batista <ronaldosnbatista@gmail.com>
2025-07-27 23:17:36 -0300

    modelo de tabela estruturado

--- encurta_url/models.py
+++ encurta_url/models.py
@@ -1,1 +1,6 @@
 from encurta_url import app, db
+
+class Url(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.String(500), nullable=False)
+    short_url = db.Column(db.String(10), unique=True, nullable=False)
